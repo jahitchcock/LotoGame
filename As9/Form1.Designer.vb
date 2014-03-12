3f2c540fe92bf034c2c154bd1ae656c116eba955
jahitchcock <joshua.hitchcock@live.com>
2014-03-11 20:36:46 -0400

    Added to Repository
    Lotto style guessing game assignment from CIS130 at HFCC

---
+++ As9/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNum1 = New System.Windows.Forms.TextBox
+        Me.txtNum2 = New System.Windows.Forms.TextBox
+        Me.txtNum3 = New System.Windows.Forms.TextBox
+        Me.txtNum4 = New System.Windows.Forms.TextBox
+        Me.txtNum5 = New System.Windows.Forms.TextBox
+        Me.txtNum6 = New System.Windows.Forms.TextBox
+        Me.btnExit = New System.Windows.Forms.Button
+        Me.btnClear = New System.Windows.Forms.Button
+        Me.btnSubmit = New System.Windows.Forms.Button
+        Me.lblBall1 = New System.Windows.Forms.Label
+        Me.lblBall2 = New System.Windows.Forms.Label
+        Me.lblBall3 = New System.Windows.Forms.Label
+        Me.lblBall4 = New System.Windows.Forms.Label
+        Me.lblBall5 = New System.Windows.Forms.Label
+        Me.lblBall6 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(12, 127)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(70, 20)
+        Me.txtNum1.TabIndex = 0
+        Me.txtNum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(82, 127)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(70, 20)
+        Me.txtNum2.TabIndex = 1
+        Me.txtNum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtNum3
+        '
+        Me.txtNum3.Location = New System.Drawing.Point(152, 127)
+        Me.txtNum3.Name = "txtNum3"
+        Me.txtNum3.Size = New System.Drawing.Size(70, 20)
+        Me.txtNum3.TabIndex = 2
+        Me.txtNum3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtNum4
+        '
+        Me.txtNum4.Location = New System.Drawing.Point(222, 127)
+        Me.txtNum4.Name = "txtNum4"
+        Me.txtNum4.Size = New System.Drawing.Size(70, 20)
+        Me.txtNum4.TabIndex = 3
+        Me.txtNum4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtNum5
+        '
+        Me.txtNum5.Location = New System.Drawing.Point(292, 127)
+        Me.txtNum5.Name = "txtNum5"
+        Me.txtNum5.Size = New System.Drawing.Size(70, 20)
+        Me.txtNum5.TabIndex = 4
+        Me.txtNum5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtNum6
+        '
+        Me.txtNum6.Location = New System.Drawing.Point(362, 127)
+        Me.txtNum6.Name = "txtNum6"
+        Me.txtNum6.Size = New System.Drawing.Size(70, 20)
+        Me.txtNum6.TabIndex = 5
+        Me.txtNum6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(292, 185)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(140, 42)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Location = New System.Drawing.Point(152, 185)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(140, 42)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        Me.btnClear.Visible = False
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(12, 185)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(140, 42)
+        Me.btnSubmit.TabIndex = 8
+        Me.btnSubmit.Text = "&Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'lblBall1
+        '
+        Me.lblBall1.AutoSize = True
+        Me.lblBall1.Location = New System.Drawing.Point(38, 150)
+        Me.lblBall1.Name = "lblBall1"
+        Me.lblBall1.Size = New System.Drawing.Size(13, 13)
+        Me.lblBall1.TabIndex = 9
+        Me.lblBall1.Text = "?"
+        '
+        'lblBall2
+        '
+        Me.lblBall2.AutoSize = True
+        Me.lblBall2.Location = New System.Drawing.Point(108, 150)
+        Me.lblBall2.Name = "lblBall2"
+        Me.lblBall2.Size = New System.Drawing.Size(13, 13)
+        Me.lblBall2.TabIndex = 10
+        Me.lblBall2.Text = "?"
+        '
+        'lblBall3
+        '
+        Me.lblBall3.AutoSize = True
+        Me.lblBall3.Location = New System.Drawing.Point(178, 150)
+        Me.lblBall3.Name = "lblBall3"
+        Me.lblBall3.Size = New System.Drawing.Size(13, 13)
+        Me.lblBall3.TabIndex = 11
+        Me.lblBall3.Text = "?"
+        '
+        'lblBall4
+        '
+        Me.lblBall4.AutoSize = True
+        Me.lblBall4.Location = New System.Drawing.Point(248, 150)
+        Me.lblBall4.Name = "lblBall4"
+        Me.lblBall4.Size = New System.Drawing.Size(13, 13)
+        Me.lblBall4.TabIndex = 12
+        Me.lblBall4.Text = "?"
+        '
+        'lblBall5
+        '
+        Me.lblBall5.AutoSize = True
+        Me.lblBall5.Location = New System.Drawing.Point(318, 150)
+        Me.lblBall5.Name = "lblBall5"
+        Me.lblBall5.Size = New System.Drawing.Size(13, 13)
+        Me.lblBall5.TabIndex = 13
+        Me.lblBall5.Text = "?"
+        '
+        'lblBall6
+        '
+        Me.lblBall6.AutoSize = True
+        Me.lblBall6.Location = New System.Drawing.Point(388, 150)
+        Me.lblBall6.Name = "lblBall6"
+        Me.lblBall6.Size = New System.Drawing.Size(13, 13)
+        Me.lblBall6.TabIndex = 14
+        Me.lblBall6.Text = "?"
+        '
+        'frmMain
+        '
+        Me.AcceptButton = Me.btnSubmit
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnClear
+        Me.ClientSize = New System.Drawing.Size(448, 239)
+        Me.Controls.Add(Me.lblBall6)
+        Me.Controls.Add(Me.lblBall5)
+        Me.Controls.Add(Me.lblBall4)
+        Me.Controls.Add(Me.lblBall3)
+        Me.Controls.Add(Me.lblBall2)
+        Me.Controls.Add(Me.lblBall1)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.txtNum6)
+        Me.Controls.Add(Me.txtNum5)
+        Me.Controls.Add(Me.txtNum4)
+        Me.Controls.Add(Me.txtNum3)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Name = "frmMain"
+        Me.Text = "Lotto"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtNum1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum4 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum5 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum6 As System.Windows.Forms.TextBox
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents lblBall1 As System.Windows.Forms.Label
+    Friend WithEvents lblBall2 As System.Windows.Forms.Label
+    Friend WithEvents lblBall3 As System.Windows.Forms.Label
+    Friend WithEvents lblBall4 As System.Windows.Forms.Label
+    Friend WithEvents lblBall5 As System.Windows.Forms.Label
+    Friend WithEvents lblBall6 As System.Windows.Forms.Label
+
+End Class
---
+++ As9/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class frmMain
+
+    Dim arLotto(61) As Integer
+    Dim lbl As New Label()
+
+    Private Sub btnSubmit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSubmit.Click
+        Dim x, matches, y As Integer
+        y = 0
+        Select Case Val(txtNum2.Text)
+            Case Is = Val(txtNum1.Text), Val(txtNum3.Text), Val(txtNum4.Text), Val(txtNum5.Text), Val(txtNum6.Text)
+                MessageBox.Show("Please enter a number only one time")
+                y = 1
+            Case Else
+                Select Case Val(txtNum3.Text)
+                    Case Is = Val(txtNum1.Text), Val(txtNum4.Text), Val(txtNum5.Text), Val(txtNum6.Text)
+                        MessageBox.Show("Please enter a number only one time")
+                        y = 1
+                End Select
+                Select Case Val(txtNum4.Text)
+                    Case Is = Val(txtNum1.Text), Val(txtNum5.Text), Val(txtNum6.Text)
+                        MessageBox.Show("Please enter a number only one time")
+                        y = 1
+                End Select
+                Select Case Val(txtNum5.Text)
+                    Case Is = Val(txtNum1.Text), Val(txtNum6.Text)
+                        MessageBox.Show("Please enter a number only one time")
+                        y = 1
+                End Select
+                Select Case Val(txtNum6.Text)
+                    Case Is = Val(txtNum1.Text)
+                        MessageBox.Show("Please enter a number only one time")
+                        y = 1
+                End Select
+
+                If y = 0 Then
+                    matches = 0
+                    For x = 1 To 6
+                        If (Val(txtNum1.Text) = arLotto(x - 1)) Then
+                            matches += 1
+                        ElseIf (Val(txtNum2.Text) = arLotto(x - 1)) Then
+                            matches += 1
+                        ElseIf (Val(txtNum3.Text) = arLotto(x - 1)) Then
+                            matches += 1
+                        ElseIf (Val(txtNum4.Text) = arLotto(x - 1)) Then
+                            matches += 1
+                        ElseIf (Val(txtNum5.Text) = arLotto(x - 1)) Then
+                            matches += 1
+                        ElseIf (Val(txtNum6.Text) = arLotto(x - 1)) Then
+                            matches += 1
+                        End If
+
+                    Next
+                    winnings(matches)
+
+                    lblBall1.Text = arLotto(0).ToString
+                    lblBall2.Text = arLotto(1).ToString
+                    lblBall3.Text = arLotto(2).ToString
+                    lblBall4.Text = arLotto(3).ToString
+                    lblBall5.Text = arLotto(4).ToString
+                    lblBall6.Text = arLotto(5).ToString
+                    btnSubmit.Visible = False
+                End If
+            End Select
+        btnClear.Visible = True
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        txtNum1.Text = ""
+        txtNum2.Text = ""
+        txtNum3.Text = ""
+        txtNum4.Text = ""
+        txtNum5.Text = ""
+        txtNum6.Text = ""
+        lblBall1.Text = "?"
+        lblBall2.Text = "?"
+        lblBall3.Text = "?"
+        lblBall4.Text = "?"
+        lblBall5.Text = "?"
+        lblBall6.Text = "?"
+        btnSubmit.Visible = True
+        btnClear.Visible = False
+        Randomize()
+
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        labels()
+        Randomize()
+    End Sub
+
+    Private Sub Randomize()
+        Dim i, x, y, temp As Integer
+        Dim rnd As New Random
+        For i = 1 To 60
+            arLotto(i) = i
+        Next
+
+        For x = 0 To (arLotto.Length - 1)
+            y = rnd.Next(0, arLotto.Length - 1)
+            temp = arLotto(x)
+            arLotto(x) = arLotto(y)
+            arLotto(y) = temp
+        Next
+    End Sub
+
+    Private Sub labels()
+        Dim a, b, c As Integer
+        Dim name As String
+
+        c = 1
+        For a = 1 To 3
+            For b = 1 To 20
+                name = ("lbl" & c.ToString)
+                Me.lbl.Text = c.ToString
+                'Me.lbl.Location = New System.Drawing.Point(b * 10, a * 10)
+                Me.lbl.Visible = True
+                Me.lbl.Name = name
+                Me.Controls.Add(lbl)
+                c += 1
+            Next
+        Next
+
+    End Sub
+    Private Sub winnings(ByVal matches)
+        Select Case matches
+            Case Is = 4
+                MessageBox.Show("You win $50!")
+            Case Is = 5
+                MessageBox.Show("You win $500!")
+            Case Is = 6
+                MessageBox.Show("You win $5000!")
+            Case Else
+                MessageBox.Show("You had " & matches & " matches!")
+
+        End Select
+    End Sub
+
+   
+End Class
